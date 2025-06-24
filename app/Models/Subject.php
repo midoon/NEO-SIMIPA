1b5364d7713b3eebbde8438d7a7c768c2940ebc3
midoon <harunkoding@gmail.com>
2025-06-24 13:28:43 +0700

    create:migrations

---
+++ app/Models/Subject.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Subject extends Model
+{
+    protected $guarded = ['id'];
+    use HasFactory;
+
+    public function schedules(): HasMany
+    {
+        return $this->hasMany(Schedule::class);
+    }
+}
---
+++ database/migrations/2025_06_24_062140_create_receipts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('receipts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('fee_id')->constrained(
+                table: 'fees',
+                indexName: 'receipt_fee_id'
+            );
+            $table->string('receipt_number')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('receipts');
+    }
+};

572b6f1a2fc1c92774acad555d630494bea5c065
midoon <harunkoding@gmail.com>
2025-08-10 10:16:47 +0700

    feat:teacher-registration

--- app/Livewire/Auth/TeacherRegister.php
+++ app/Livewire/Auth/TeacherRegister.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\Teacher;
+use Illuminate\Validation\ValidationException;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
@@ -14,6 +16,12 @@ class TeacherRegister extends Component
     public $nik = "";
     public $password = "";
     public $confirm_password = "";
+
+    protected $rules = [
+        'nik' => 'required|numeric',
+        'password' => 'required|min:8',
+        'confirm_password' => 'required|min:8',
+    ];
     public function render()
     {
         return view('livewire.auth.teacher-register');
@@ -21,6 +29,45 @@ class TeacherRegister extends Component
 
     public function register()
     {
-        dd("register");
+        try {
+
+            $this->validate();
+
+
+            $isMatchPassword = $this->password === $this->confirm_password;
+            if (!$isMatchPassword) {
+                session()->flash('error', 'Password dan konfirmasi password tidak cocok.');
+                return $this->redirect('/teacher/register', navigate: true);
+            }
+
+            $teacher = Teacher::where('nik', $this->nik)->first();
+            if (!$teacher) {
+                session()->flash('error', 'NIK tidak ditemukan.');
+                return $this->redirect('/teacher/register', navigate: true);
+            }
+
+            if ($teacher->account) {
+                session()->flash('error', 'NIK sudah memiliki akun.');
+                return $this->redirect('/teacher/register', navigate: true);
+            }
+
+            $teacher->update([
+                'password' => bcrypt($this->password),
+                'account' => true,
+            ]);
+
+
+
+
+            session()->flash('success', 'Success membuat akun guru. Silakan login.');
+            return $this->redirect('/teacher/login', navigate: true);
+        } catch (\Exception $e) {
+            if ($e instanceof ValidationException) {
+                $messages = collect($e->errors())->flatten()->implode(' ');
+                session()->flash('error', 'Registration failed. ' . $messages);
+            } else {
+                session()->flash('error', 'Registration failed. ' . $e->getMessage());
+            }
+        }
     }
 }
--- resources/views/livewire/auth/teacher-register.blade.php
+++ resources/views/livewire/auth/teacher-register.blade.php
@@ -1,4 +1,36 @@
 <div class="bg-gradient-to-r from-simipa-3  to-simipa-4 w-full">
+
+    {{-- notifikasi --}}
+    @if (session()->has('success'))
+        <div x-data="{ show: false }" x-init="setTimeout(() => show = true, 500);
+        setTimeout(() => show = false, 10500)" x-show="show"
+            x-transition:enter="transform transition ease-out duration-300"
+            x-transition:enter-start="translate-x-full opacity-0" x-transition:enter-end="translate-x-0 opacity-100"
+            x-transition:leave="transform transition ease-in duration-300"
+            x-transition:leave-start="translate-x-0 opacity-100" x-transition:leave-end="translate-x-full opacity-0"
+            class="fixed top-4 right-4 z-50 bg-green-500 text-white ml-4 px-4 py-2 rounded shadow flex items-center justify-between space-x-4">
+            <span>{{ session('success') }}</span>
+            <button @click="show = false" class="text-white hover:text-gray-200 font-bold">
+                &times;
+            </button>
+        </div>
+    @endif
+
+    @if (session()->has('error'))
+        <div x-data="{ show: false }" x-init="setTimeout(() => show = true, 500);
+        setTimeout(() => show = false, 10500)" x-show="show"
+            x-transition:enter="transform transition ease-out duration-300"
+            x-transition:enter-start="translate-x-full opacity-0" x-transition:enter-end="translate-x-0 opacity-100"
+            x-transition:leave="transform transition ease-in duration-300"
+            x-transition:leave-start="translate-x-0 opacity-100" x-transition:leave-end="translate-x-full opacity-0"
+            class="fixed top-4 right-4 z-50 bg-red-500 text-white ml-4 px-4 py-2 rounded shadow flex items-center justify-between space-x-4">
+            <span>{{ session('error') }}</span>
+            <button @click="show = false" class="text-white hover:text-gray-200 font-bold">
+                &times;
+            </button>
+        </div>
+    @endif
+
     <div class="w-full flex justify-center items-center min-h-screen px-8">
         <div class="w-full md:max-w-sm rounded-xl bg-simipa-5 p-10 ">
             {{-- @if (session('error'))
@@ -15,12 +47,18 @@
                     <input type="nik" name="nik" id="nik" placeholder="nik"
                         class=" border-1 rounded-md px-2 w-full focus:outline-none py-1.5  mt-2 focus:border-simipa-1  focus:ring-simipa-1 focus:ring-2"
                         required wire:model="nik">
+                    @error('nik')
+                        <span class="text-red-500 text-sm">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="mb-2">
                     <label for="password" class="font-bold text-simipa-3">Password</label>
                     <input type="password" name="password" id="password" placeholder="password"
                         class=" border-1 rounded-md px-2 w-full focus:outline-none py-1.5  mt-2 focus:border-simipa-1  focus:ring-simipa-1 focus:ring-2"
                         required wire:model="password">
+                    @error('password')
+                        <span class="text-red-500 text-sm">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="mb-10">
                     <label for="confirm_password" class="font-bold text-simipa-3">Konfirmasi Password</label>
@@ -28,6 +66,9 @@
                         placeholder="konfirmasi password"
                         class=" border-1 rounded-md px-2 w-full focus:outline-none py-1.5  mt-2 focus:border-simipa-1  focus:ring-simipa-1 focus:ring-2"
                         required wire:model="confirm_password">
+                    @error('confirm_password')
+                        <span class="text-red-500 text-sm">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="w-full text-center">
                     <button type="submit"

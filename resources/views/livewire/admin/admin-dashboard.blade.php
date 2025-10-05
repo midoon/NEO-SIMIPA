b421659cc6b6f1033ae87cc5238b46b1d3258858
midoon <harunkoding@gmail.com>
2025-10-05 09:20:16 +0700

    temporary dashboard admin

--- resources/views/livewire/admin/admin-dashboard.blade.php
+++ resources/views/livewire/admin/admin-dashboard.blade.php
@@ -1,4 +1,4 @@
-<div>
-    <h1>Hello admin</h1>
-
+<div class="flex flex-col items-center justify-center min-h-screen">
+    <h1 class="text-5xl font-bold text-slate-400">Selamat Datang di:</h1>
+    <img src="{{ asset('img/logo-1.PNG') }}" alt="logo.png" width="550">
 </div>
